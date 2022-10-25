7353c6e7eef585d74e62a3337b23058d9847708c
Abdulaziz Al-Ademi <abdulaziz.alademi@tu-dortmund.de>
2022-10-25 19:56:13 +0200

    Update

--- src/main/kotlin/entity/World.kt
+++ src/main/kotlin/entity/World.kt
@@ -4,7 +4,7 @@ class World (
     val middleCards : List<Card> ,
     val moveCount : Int = 0 ,
     val passCount : Int = 0 ,
-    val hasPlayerKnocked : Boolean,
+    val hasPlayerKnocked : Boolean = false,
     val players : List<Player>,
     val cardDeck: Array<Card?> = arrayOfNulls<Card>(32)
     )
