1deec6c1824951345d9c1aa98c4df0060228d1a9
Abdulaziz Al-Ademi <abdulaziz.alademi@tu-dortmund.de>
2022-10-25 19:04:29 +0200

    Update

--- src/main/kotlin/entity/World.kt
+++ src/main/kotlin/entity/World.kt
@@ -1,5 +1,11 @@
 package entity
 
-class World {
+class World (
+    var middleCards : List<Card> ,
+    var moveCount : Int ,
+    var passCount : Int ,
+    var hasPlayerKnocked : Boolean
+    )
+{
 
 }
